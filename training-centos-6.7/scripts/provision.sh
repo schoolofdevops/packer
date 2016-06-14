d5555749ff91f4f39b9baff2f1fdaba5ab19de8a
ashwini9860 <auchoudhari@gmail.com>
2016-06-14 19:00:14 +0530

    added training-centos-6.7  packer dir

---
+++ training-centos-6.7/scripts/provision.sh
@@ -0,0 +1,4 @@
+# do not disable existing repos
+#mkdir /etc/yum.repos.d/locked_repo
+#mv  /etc/yum.repos.d/*.repo /etc/yum.repos.d/locked_repo
+#mv /etc/yum.repos.d/locked_repo/localrepo.repo /etc/yum.repos.d/
